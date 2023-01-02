90a3dc48b974fae80a1fb37e100a8523e0f6ceec
secondary-jcav <secondary.jcav@gmail.com>
2023-01-02 12:18:52 +0100

    2 http test added to gif service

--- src/app/gifs/search/search.component.spec.ts
+++ src/app/gifs/search/search.component.spec.ts
@@ -1,11 +1,13 @@
 import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { GifsService } from '../services/gifs.service';
 
 import { SearchComponent } from './search.component';
 
 describe('SearchComponent', () => {
   let component: SearchComponent;
   let fixture: ComponentFixture<SearchComponent>;
+  let service: GifsService;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -21,4 +23,12 @@ describe('SearchComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  //it calls find gifs when user enters a value
+  // it('calls findGifs when a value is entered', () => {
+  //   const query = 'test';
+  //   const spy1 = jest.spyOn(service, 'findGifs').mockImplementation(() => null);
+  //   component.txtSearch.nativeElement.value = query;
+  //   expect(spy1).toHaveBeenCalledTimes(1);
+  // });
 });
--- src/app/gifs/services/gifs.service.spec.ts
+++ src/app/gifs/services/gifs.service.spec.ts
@@ -1,19 +1,53 @@
-import { HttpClientModule } from '@angular/common/http';
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { GifsService } from './gifs.service';
+import secrets from 'secrets';
 
 describe('GifsService', () => {
   let service: GifsService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientModule],
+      imports: [HttpClientTestingModule],
+      providers: [GifsService],
     });
     service = TestBed.inject(GifsService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    localStorage.clear();
+    jest.resetAllMocks();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  //it should increase the historical array
+
+  it('should add to the _history array', () => {
+    const query = 'test_query';
+    expect(service.historical.length).toBe(0);
+    service.findGifs(query);
+    expect(service.historical.length).toBe(1);
+    expect(service.historical).toContain(query);
+  });
+
+  //it should increase the results array
+
+  //it should launch a GET
+  it('results should conform to interface', () => {
+    const query = 'test_query';
+    const url = `https://api.giphy.com/v1/gifs/search?api_key=${secrets.api_key}&limit=10&q=${query}`;
+    service.findGifs(query);
+
+    const req = httpMock.expectOne(url);
+    expect(req.request.method).toBe('GET');
   });
 });
